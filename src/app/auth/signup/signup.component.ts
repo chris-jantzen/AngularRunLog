f604aa96ba628f6ab21b3e94b311a7a0c26a0dff
Chris Jantzen <jantzeca@mail.uc.edu>
2020-09-04 23:42:00 -0400

    Mock submission for creating an account

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { timer } from 'rxjs'; // TEMP
 
 @Component({
   selector: 'app-signup',
@@ -9,6 +10,10 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class SignupComponent implements OnInit {
 
   myForm: FormGroup;
+
+  // Form State After Submission
+  loading = false;
+  success = false;
 
   constructor(private fb: FormBuilder) { }
 
@@ -38,6 +43,26 @@ export class SignupComponent implements OnInit {
 
   get password() {
     return this.myForm.get('password');
+  }
+
+  async onSubmit() {
+    this.loading = true; // TODO: Some sort of loading animation. Three dots bouncing or something
+
+    const { email, password } = this.myForm.value;
+
+    try {
+      /* if after validation, it is successful, save the token somewhere (auth state probably?) and 
+      use the router to redirect to /home or /admin home if the user is an admin */
+      // await this.authservice.createAccount(email, password).subscribe(); // import the angular http library
+      console.log('sending...');
+      timer(2000).subscribe(() => {
+        console.log(email, password);
+        this.success = true;
+        console.log('successful');
+      });
+    } catch (err) {
+      console.error(err);
+    }
   }
 
 }
